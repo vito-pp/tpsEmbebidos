b8b496c20556cdfcb166c3a4f9c6cf27e7b16566
Facu Juli <fjuli@itba.edu.ar>
2025-09-13 01:47:15 -0300

    turnonled turn off led, preview of pwm, dispclear, change on display function

--- tp1/code_tp1/source/display.c
+++ tp1/code_tp1/source/display.c
@@ -10,13 +10,95 @@
  *  Author: mGonzalo Louzao
  */
 #include "drv/shift_registers.h"
+#include "display.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+
+uint8_t pwm;
+uint16_t serialCom;
+
 
 int display_init(void)
 {
+	serialCom = 0;
+	pwm = 0;
 	return serialData_init();
 }
 
-int display(uint8_t num, uint8_t disp)
+void setPWM(uint8_t desired_pwm)
+{
+	pwm = desired_pwm %4;
+}
+
+void display(unsigned int number)
+{
+	int i, j, aux = number, length = 0;
+
+	//Counts numbers' length (quantity of digits)
+	while(aux)
+	{
+		aux = aux/10;
+		length++;
+	}
+
+	aux = number;
+	for(i = 0; i < length; i++)
+	{
+		//Splits 'ON' time into 4 pieces
+		for(j = 0; j < 4; j ++)
+		{
+			if(j < pwm) // turns led (PWM/4 * 100)% of the time
+			{
+				displayDigit(aux % 10, i % 4);
+			}
+			else
+			{
+				dispClear();
+			}
+		}
+	}
+}
+void dispClear(void)
+{
+	serialCom &= 0x3000;
+	sendSerialData(serialCom);
+	return;
+}
+
+bool turnOnLED(uint8_t led)
+{
+	if(led>3)
+	{
+		return 0; //Invalid led
+	}
+	switch(led)
+	{
+		case 0: serialCom |= 0x1000 ; break;
+		case 1: serialCom |= 0x2000 ; break;
+		case 2: serialCom |= 0x3000 ; break;
+	}
+	sendSerialData(serialCom);
+	return 1;
+
+}
+
+bool turnOffLED(uint8_t led)
+{
+	if(led>3)
+		{
+			return 0; //Invalid led
+		}
+		switch(led)
+		{
+			case 0: serialCom &= ~0x1000 ; break;
+			case 1: serialCom &= ~0x2000 ; break;
+			case 2: serialCom &= ~0x3000 ; break;
+		}
+		sendSerialData(serialCom);
+		return 1;
+}
+bool displayDigit(uint8_t num, uint8_t disp)
 {
 	//int data = bcd7Seg(num%10);
 	if(num >9 || disp >3)
@@ -36,14 +118,16 @@ int display(uint8_t num, uint8_t disp)
 		    0x6F0  // 9
 	};
 
+	serialCom &= 0x3000; //Resets all bits except led ones.
 	switch(disp)
 	{
-		case 0: sendSerialData(bcd_to_7seg[num]);break;
-		case 1: sendSerialData(0x4000 | bcd_to_7seg[num]);break;
-		case 2: sendSerialData(0x8000 | bcd_to_7seg[num]);break;
-		case 3: sendSerialData(0xC000 | bcd_to_7seg[num]);break;
+		case 0: serialCom |=  (bcd_to_7seg[num]);break;
+		case 1: serialCom |= (0x4000 | bcd_to_7seg[num]);break;
+		case 2: serialCom |= (0x8000 | bcd_to_7seg[num]);break;
+		case 3: serialCom |= (0xC000 | bcd_to_7seg[num]);break;
 		default: break;
 	}
+	sendSerialData(serialCom);
 
 	return 1;
 }
--- tp1/code_tp1/source/display.h
+++ tp1/code_tp1/source/display.h
@@ -14,6 +14,9 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 /**
  * @brief Initialize Display (through serial data initialization)
  * @return TRUE if correctly initialized
@@ -22,12 +25,42 @@
 int display_init(void);
 
 /**
+ * @brief Displays number on all 4 displays
+ * @param number: number to be displayed
+ */
+void display(unsigned int number);
+
+/**
+ * @brief Clears display
+ */
+void dispClear(void);
+/**
+ * @brief Sets pwm value
+ */
+void setPWM(uint8_t desired_pwm);
+/**
  * @brief Writes character to indexed display. (0-9 or '-')
  * @param num: character to be written
  * @param disp: index of display for number to be displayed (0,1,2,3)
  * @return TRUE: correctly displayed character
  * @return FALSE: invalid character
  */
-int display(uint8_t num, uint8_t disp);
+bool displayDigit(uint8_t num, uint8_t disp);
+
+/**
+ * @brief Turns led on
+ * @param led: led to be turned on
+ * @return TRUE led correctly turned on
+ * @return FALSE failed to turn led on
+ */
+bool turnOnLED(uint8_t led);
+
+/**
+ * @brief Turns led off
+ * @param led: led to be turned off
+ * @return TRUE led correctly turned off
+ * @return FALSE failed to turn led off
+ */
+bool turnOffLED(uint8_t led);
 
 #endif
