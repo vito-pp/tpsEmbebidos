2e87343b0af4e86e75f9efee310751de8880c17f
Santino Agosti <santinoaggosti@gmail.com>
2025-09-15 16:19:24 -0300

    Formato correcto de rotary_encoder.h

--- tp1/code_tp1/source/drv/rotary_encoder.h
+++ tp1/code_tp1/source/drv/rotary_encoder.h
@@ -1,22 +1,40 @@
+/**
+ * @file rotary_encoder.h
+ * @brief Archivo header exlusivo al manejo y logica del encoder rotativo y su boton.
+ */
+
 #ifndef ROTARY_ENCODER_H
 #define ROTARY_ENCODER_H
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
 
 #include <stdbool.h>
 #include <stdint.h>
 
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
 #define ENCODER_BUFFER_SIZE 5
 #define MIN_PRESS_CYCLES 50
 
-#define LONG_PRESS_TIME 2 //Seconds
-#define INTERRUPT_FREQUENCY 2000 //Hz
+#define LONG_PRESS_TIME 2        // Seconds
+#define INTERRUPT_FREQUENCY 2000 // Hz
 
 #define MAX_DOUBLE_PRESS_COOLDOWN 1 // Seconds
 
-#define LONG_PRESS_CYCLES ((INTERRUPT_FREQUENCY * LONG_PRESS_TIME) \
-                            *ENCODER_BUFFER_SIZE) //Ciclos
-#define MAX_CYCLES_BETWEEN_DOUBLE_PRESS 1000 //Ciclos
+#define LONG_PRESS_CYCLES ((INTERRUPT_FREQUENCY * LONG_PRESS_TIME) * ENCODER_BUFFER_SIZE) // Ciclos
+#define MAX_CYCLES_BETWEEN_DOUBLE_PRESS 1000                                              // Ciclos
 
-typedef enum {
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+typedef enum
+{
     ENC_NONE = 0,
     ENC_CW,
     ENC_CCW,
@@ -25,9 +43,30 @@ typedef enum {
     ENC_BUTTON_LONG_PRESS
 } enc_input_t;
 
+
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/**
+ * @brief Lectura y logica del estado del encoder rotativo y su boton. 
+ * @return Estado actual del encoder. Priorizacion de eventos: Double Press > Long Press > Press > CW = CCW
+ */
 enc_input_t encoder_update(void);
+
+/**
+ * @brief Callback de interrupcion para analisis de encoder. "Flag up"
+ */
 void encoder_callback(void);
+
+/**
+ * @brief Seteo de pines de lectura de Encoder (rotativo y boton)
+ */
 void encoderInit(void);
+
+/*******************************************************************************
+ ******************************************************************************/
 
 
 #endif // ROTARY_ENCODER_H
