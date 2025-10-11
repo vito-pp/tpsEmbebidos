f537d4c54e7b53b22483917eadd30e597b8acb8e
Facu Juli <fjuli@itba.edu.ar>
2025-10-11 17:30:26 -0300

    definicion de base ptr adrresses of spi

--- tp2/code_tp2/src/drv/spi.c
+++ tp2/code_tp2/src/drv/spi.c
@@ -1,10 +1,27 @@
 #include "spi.h"
 #include <stdio.h>
+#include "../CMSIS/MK64F12.h"
+#define FRAME_SIZE 8
 
+static SPI_Type* const spi_adresses[] = SPI_BASE_PTRS;
 
 bool SPI_init(void)
 {
+    //Config Ctars
+    //spi0 (4 tx fifo y 4 rx fifo)
+    //Wake up mode? no, se puede perder first word
+    //Spi _SR (interrupt)
+    
+}
 
+void configCTAR0(uint8_t x)
+{
+    //if CURRENT TRASNFERING
+    //          RETURN
+    // DO NOT CONGI CTAR WHILE TRANSFER
+    SPI_Type* spix = spi_adresses[x];
+
+    
 }
 
 bool isSPIDataReady(void)
--- tp2/code_tp2/src/drv/spi.h
+++ tp2/code_tp2/src/drv/spi.h
@@ -6,6 +6,8 @@
 
 bool SPI_init(void);
 
+void configCTAR0(uint8_t x);
+
 bool isSPIDataReady(void);
 
 uint64_t getSPIData(void);
