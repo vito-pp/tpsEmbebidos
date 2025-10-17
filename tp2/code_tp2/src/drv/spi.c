3a47d62aa3205b8ec0b65e30cc4bfd754b0c91e4
Facu Juli <fjuli@itba.edu.ar>
2025-10-17 19:10:11 -0300

    drivers adaptados

--- tp2/code_tp2/src/drv/spi.c
+++ tp2/code_tp2/src/drv/spi.c
@@ -1,32 +1,160 @@
 #include "spi.h"
 #include <stdio.h>
 #include "../CMSIS/MK64F12.h"
-#include "board.h"
 #include "gpio.h"
+#include "board.h"
 
 
 #define FRAME_SIZE 8
-#define SPI_INSTANCES 3
 #define CTAR_OPTIONS 2
-#define TX_FIFO_SIZE 10
+#define TX_BUFFER_SIZE 30
+#define RX_BUFFER_SIZE 30
 
 static SPI_Type* const spi_base_adress[] = SPI_BASE_PTRS;
 
 static PORT_Type * const kPort[] = PORT_BASE_PTRS;
 
-static uint16_t tx_buffer[TX_FIFO_SIZE];
+static uint32_t tx_buffer[TX_BUFFER_SIZE];
+static uint32_t rx_buffer[RX_BUFFER_SIZE];
 
-int SPIx_pushTx(uint8_t spi_instance, uint16_t data, uint8_t pcs, uint8_t ctar_x, bool ctcnt)
+/**
+ * @brief Pushes data to the SPI0 TX FIFO with specified configuration.
+ * @param data The 16-bit data to be transmitted via the SPI interface.
+ * @param pcs The Peripheral Chip Select (PCS) signal to be used for the transaction.
+ * @param ctar_x The Configuration Register (CTAR) index to define SPI transfer settings.
+ * @param ctcnt Clear Transfer Counter (true to clear, false to not clear).
+ * @return 1 on success (data successfully pushed to the transmit buffer).
+ */
+static int SPI0_pushTx(uint16_t data, uint8_t pcs, bool ctcnt);
+
+/**
+ * @brief Configures the multiplexing and interrupt settings for a specified pin.
+ * @param pin The pin to be configured.
+ * @param alt The alternate function (MUX) to assign to the pin (e.g., GPIO, SPI, UART).
+ * @param irqc The interrupt configuration for the pin (e.g., interrupt type or disabled).
+ * @return None (void function, no return value).
+ */
+static void pinConfig(uint8_t pin, uint8_t alt, bool irqc);
+
+void pushTxRoundedBuffer(uint16_t data);
+
+void SPI0Master_Init(void)
 {
+    int i = 0;
+    for(i= 0; i < TX_BUFFER_SIZE; i++)
+    {
+        tx_buffer[i] = 0xFFFFFFFF;
+    }
 
-    //REvisar cont 
-    //Validate all
-    spi_base_adress[spi_instance]->PUSHR = (SPI_PUSHR_TXDATA(data)
-                                        | SPI_PUSHR_CTAS(ctar_x)
-                                        | SPI_PUSHR_CTCNT(ctcnt &&1));
-    return 1;
+    bool cont_scke = 0;
+    bool rooe = 0;
+    bool pcsis = 0;
+
+    //enables clock gating
+    SIM->SCGC6 |= SIM_SCGC6_SPI0_MASK;
+
+    pinConfig(SPI0_SIN, ALT2, 0);
+    pinConfig(SPI0_SOUT, ALT2, 0);
+    pinConfig(SPI0_SCLK, ALT2, 0);
+    pinConfig(SPI0_PCS0, ALT2, 0);
+
+    SPI_Type* spi_x = spi_base_adress[0];
+
+    //Puede que falte habilitar mdis
+    //MCR CONFIGURATION
+    spi_x->MCR |= SPI_MCR_MSTR(1) | SPI_MCR_CONT_SCKE(cont_scke &&1) |
+                    SPI_MCR_ROOE(rooe &&1) | SPI_MCR_PCSIS(pcsis &&1) |
+                    SPI_MCR_PCSIS(pcsis &&1) | SPI_MCR_CLR_RXF(1) |
+                    SPI_MCR_CLR_TXF(1);
+
+    //CTAR0 CONFIGURATION
+    bool cpol = 0;
+    uint8_t cpha = 0;
+    uint8_t lsfe = 0;
+    uint8_t fmsz = 7;
+    uint16_t br= 125;
+
+    spi_x->CTAR[0] |= SPI_CTAR_FMSZ(fmsz &&1);
+    spi_x->CTAR[0] |= SPI_CTAR_CPOL(cpol &&1);
+    spi_x->CTAR[0] |= SPI_CTAR_CPHA(cpha &&1);
+    spi_x->CTAR[0] |= SPI_CTAR_LSBFE(lsfe &&1);
+
+    switch(br)
+    {
+        case 125: spi_x->CTAR[0] |= SPI_CTAR_PBR(5) | SPI_CTAR_BR(16); break;
+        case 500: spi_x->CTAR[0] |= SPI_CTAR_PBR(5) | SPI_CTAR_BR(4); break;
+        case 1000: spi_x->CTAR[0] |= SPI_CTAR_PBR(5) | SPI_CTAR_BR(2); break;
+        case 2500: spi_x->CTAR[0] |= SPI_CTAR_PBR(2) | SPI_CTAR_BR(2); break;
+        default: break;
+    }
+    
 
 }
+
+
+void pinConfig(uint8_t pin, uint8_t alt, uint8_t irqc)
+{
+    kPort[PIN2PORT(pin)]->PCR[PIN2NUM(pin)] &= ~(PORT_PCR_MUX(0b111) | 
+                                            PORT_PCR_IRQC(0b1111));
+    kPort[PIN2PORT(pin)]->PCR[PIN2NUM(pin)] |= PORT_PCR_MUX(alt);
+    kPort[PIN2PORT(pin)]->PCR[PIN2NUM(pin)] |= PORT_PCR_IRQC(irqc);
+}
+
+void SPI0_sendByte(uint16_t data_1)
+{
+    pushTxRoundedBuffer(data_1,0);
+    
+}
+void SPI0_send3Bytes(uint16_t data_1, uint16_t data_2, uint16_t data_3)
+{
+    pushTxRoundedBuffer(data_1,1);
+    pushTxRoundedBuffer(data_2,1);
+    pushTxRoundedBuffer(data_3,0);
+    
+}
+
+void pushTxRoundedBuffer(uint16_t data, bool cont)
+{
+    static int index = 0;
+    tx_buffer[index] = data | SPI_PUSHR_CONT(cont) |SPI_PUSHR_PCS(1);
+    index = (index + 1) % TX_BUFFER_SIZE;
+}
+
+void SPI0_PopRxFIFO(void)
+{
+    static int index = 0;
+    uint16_t aux;
+
+
+
+}
+
+
+void SPI0_PushTx_IRQ(void)
+{
+    static int i = 0;
+    if(tx_buffer[i] != 0xFFFFFFFF)
+    {
+        spi_base_adress[0]->PUSHR = tx_buffer[i];
+        tx_buffer[i] = 0xFFFFFFFF;
+        i = (i + 1) % TX_BUFFER_SIZE;
+    }
+    
+}
+
+void SPI0_PopRx_IRQ(void)
+{
+    static int i = 0;
+    if(tx_buffer[i] != 0xFFFFFFFF)
+    {
+        spi_base_adress[0]->PUSHR = tx_buffer[i];
+        tx_buffer[i] = 0xFFFFFFFF;
+        i = (i + 1) % TX_BUFFER_SIZE;
+    }
+    
+}
+
+
 
 
 uint32_t SPIx_popRx(uint8_t spi_instance)
@@ -40,96 +168,3 @@ uint32_t SPIx_TXFR0(uint8_t spi_instance)
 }
 
 //fifo es de 4 x fmsz
-
-void SPI0_Init(SPI_pins* pins, bool mstre, bool cont_scke, bool rooe,
-             bool pcsis)
-{
-    pins->SIN = PORTNUM2PIN(PD, 3);
-    pins->SOUT = PORTNUM2PIN(PD, 2);
-    pins->SCLK = PORTNUM2PIN(PD, 1);
-    pins->PCS0 = PORTNUM2PIN(PD, 0);
-
-    SIM->SCGC6 |= SIM_SCGC6_SPI0_MASK;
-
-    pinConfig(pins->SIN, ALT2, 0);
-    pinConfig(pins->SOUT, ALT2, 0);
-    pinConfig(pins->SCLK, ALT2, 0);
-    pinConfig(pins->PCS0, ALT2, 0);
-
-    SPI_Type* spi_x = spi_base_adress[0];
-
-    spi_x->MCR |= SPI_MCR_MSTR(mstre &&1) | SPI_MCR_CONT_SCKE(cont_scke &&1) |
-                    SPI_MCR_ROOE(rooe &&1) | SPI_MCR_PCSIS(pcsis &&1) |
-                    SPI_MCR_PCSIS(pcsis &&1) | SPI_MCR_CLR_RXF(1) |
-                    SPI_MCR_CLR_TXF(1);
-}
-
-
-void pinConfig(uint8_t pin, uint8_t alt, uint8_t irqc)
-{
-    kPort[PIN2PORT(pin)]->PCR[PIN2NUM(pin)] &= ~(PORT_PCR_MUX(0b111) | 
-                                            PORT_PCR_IRQC(0b1111));
-    kPort[PIN2PORT(pin)]->PCR[PIN2NUM(pin)] |= PORT_PCR_MUX(alt);
-    kPort[PIN2PORT(pin)]->PCR[PIN2NUM(pin)] |= PORT_PCR_IRQC(irqc);
-}
-
-
-
-int CTARX_config(uint8_t spi_instance, uint8_t ctar_x, uint8_t fmsz, 
-                    bool cpol , uint8_t cpha, uint8_t lsfe, uint16_t br)
-{
-    if(spi_instance >= SPI_INSTANCES || ctar_x >= CTAR_OPTIONS)
-    {
-        return -1;
-    }
-    SPI_Type* spi_x = spi_base_adress[spi_instance];
-
-    if(fmsz > 15 || fmsz < 4)
-    {
-        return 1;
-    }
-    
-    spi_x->CTAR[ctar_x] |= SPI_CTAR_FMSZ(fmsz &&1);
-    spi_x->CTAR[ctar_x] |= SPI_CTAR_CPOL(cpol &&1);
-    spi_x->CTAR[ctar_x] |= SPI_CTAR_CPHA(cpha &&1);
-    spi_x->CTAR[ctar_x] |= SPI_CTAR_LSBFE(lsfe &&1);
-
-    switch(br)
-    {
-        case 125: spi_x->CTAR[ctar_x] |= SPI_CTAR_PBR(5) | SPI_CTAR_BR(16); break;
-        case 500: spi_x->CTAR[ctar_x] |= SPI_CTAR_PBR(5) | SPI_CTAR_BR(4); break;
-        case 1000: spi_x->CTAR[ctar_x] |= SPI_CTAR_PBR(5) | SPI_CTAR_BR(2); break;
-        case 2500: spi_x->CTAR[ctar_x] |= SPI_CTAR_PBR(2) | SPI_CTAR_BR(2); break;
-        default: break;
-    }
-
-    //asignar pccssck y pasc y esos
-    
-    return 0;
-}
-
-int getStatus(void)
-{
-    //SPI_SR
-}
-bool isSPIDataReady(void)
-{
-
-}
-
-uint64_t getSPIData(void)
-{
-
-}
-
-int isTxOver(void)
-{
-
-}
-
-bool sendSPIData(uint64_t data)
-{
-
-}
-
-
--- tp2/code_tp2/src/drv/spi.h
+++ tp2/code_tp2/src/drv/spi.h
@@ -4,25 +4,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct {
-    uint8_t SIN;
-    uint8_t SOUT;
-    uint8_t SCLK;
-    uint8_t PCS0;
-}SPI_pins;
 
 /**
- * @brief Pushes data to the SPI TX FIFO with specified configuration.
- * @param spi_instance The SPI module instance to be used (e.g., 0 for SPI0, 1 for SPI1).
- * @param data The 16-bit data to be transmitted via the SPI interface.
- * @param pcs The Peripheral Chip Select (PCS) signal to be used for the transaction.
- * @param ctar_x The Configuration Register (CTAR) index to define SPI transfer settings.
- * @param eoq Indicates whether this is the End of Queue (EOQ) 
- *              for the transfer (true for EOQ, false otherwise).
- * @param ctcnt Clear Transfer Counter (true to clear, false to not clear).
- * @return 1 on success (data successfully pushed to the transmit buffer).
+ * @brief Configures the pins for the SPI0 to be used as masters.
+ *        Used pins are specified in board.h file.
+ * @return None.
  */
-int SPIx_pushTx(uint8_t spi_instance, uint16_t data, uint8_t pcs, uint8_t ctar_x, bool ctcnt);
+void SPI0Master_Init(void);
+
+void SPI0_PushTx_IRQ(void);
+
+void SPI0_PopRx_IRQ(void);
 
 /**
  * @brief Pops data from the SPI RX FIFO.
@@ -31,71 +23,11 @@ int SPIx_pushTx(uint8_t spi_instance, uint16_t data, uint8_t pcs, uint8_t ctar_x
  */
 uint32_t SPIx_popRx(uint8_t spi_instance);
 
-/**
- * @brief For debug purposes: Returns data contained in SPI TX FIFO
- *          It does not pop/push data to the FIFO.
- * @param spi_instance The SPI module instance to be used (e.g., 0 for SPI0, 1 for SPI1).
- * @return The 32-bit SPI data to be shifted out
- */
-uint32_t SPIx_TXFR0(uint8_t spi_instance);
+void SPI0_sendByte(uint16_t data_1);
 
-/**
- * @brief Configures the pins for the SPI0 module, and the Module Configuration Register (MCR).
- * @param pins Pointer to an SPI_pins structure containing
- *             the pin assignments for SPI0 (SIN, SOUT, SCLK, PCS0).
- * @param mstre Specifies whether the SPI operates in master mode (true) or slave mode (false).
- * @param cont_scke Enables the Serial Communication Clock (SCK) to run continuously.
- * @param rooe Enables Receive FIFO Overflow Overwrite
- * @param pcsis Sets the Peripheral Chip Select (PCS) signals inactive state. 
- *              FALSE -->  low , TRUE --> high
- * @return None.
- */
-void SPI0_Init(SPI_pins* pins, bool mstre, bool cont_scke, bool rooe, bool pcsis);
-
-/**
- * @brief Configures the multiplexing and interrupt settings for a specified pin.
- * @param pin The pin to be configured.
- * @param alt The alternate function (MUX) to assign to the pin (e.g., GPIO, SPI, UART).
- * @param irqc The interrupt configuration for the pin (e.g., interrupt type or disabled).
- * @return None (void function, no return value).
- */
-void pinConfig(uint8_t pin, uint8_t alt, bool irqc);
-
-/**
- * @brief Configures the Clock and Transfer Attributes Register (CTAR) for the specified SPI instance.
- * @param spi_instance The SPI module instance to be configured (e.g., 0 for SPI0, 1 for SPI1).
- * @param ctar_x The CTAR register index to configure (e.g., 0 for CTAR0, 1 for CTAR1).
- * @param fmsz The frame size in bits for the SPI transfer.
- *              Real Frame Size = fmsz + 1,  ( 4 <= fmsz <= 15 bits)
- * @param cpol Selects the inactive state of the Serial Communications Clock (SCK)
- *          TRUE  --> Inactive Low
- *          FALSE --> Inactive High
- * @param cpha The clock phase.
- *          FALSE --> Data is captured on the leading edge of SCK and changed on the following edge.
- *          TRUE --> Data is changed on the leading edge of SCK and captured on the following edge.
- * @param lsfe Specifies the bit order.
- *          TRUE  --> for LSB first 
- *          FASLE --> for MSB first
- * @param br BR value in 10 kbps ( Bps = br . 10k bps) options (125, 5, 10, 25)
- * @return 0 on success 
- *         -1 if spi_instance or ctar_x is invalid
- *          1 if fmsz is out of valid range (4-15).
- */
-int CTARX_config(uint8_t spi_instance, uint8_t ctar_x, uint8_t fmsz, 
-                    bool cpol , uint8_t cpha, uint8_t lsfe, uint16_t br);
+void SPI0_send3Bytes(uint16_t data_1, uint16_t data_2, uint16_t data_3);
 
 
-
-
-bool SPI_init(void);
-
-bool isSPIDataReady(void);
-
-uint64_t getSPIData(void);
-
-int isTxOver(void);
-
-bool sendSPIData(uint64_t data);
 
 
 
